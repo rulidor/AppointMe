bcd2748229c330742ae2ae24adea4c27db8b66ba
rulidor <rulidor90@gmail.com>
2021-07-28 11:04:45 +0300

    created login UI

--- app/src/main/java/com/example/appointme/MainActivity.java
+++ app/src/main/java/com/example/appointme/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.appointme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+
+    private EditText name, password;
+    private Button buttonLogin;
+    private CheckBox prefCheckbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        name = (EditText)findViewById(R.id.)
     }
 }
