2bee4291f8c1527913ee644639c81882dafc8e1a
rulidor <rulidor90@gmail.com>
2021-07-30 11:37:27 +0300

    updated register

--- app/src/main/java/com/example/appointme/RegisterActivity.java
+++ app/src/main/java/com/example/appointme/RegisterActivity.java
@@ -2,13 +2,24 @@ package com.example.appointme;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private TextView regUsername;
+    private TextView regPassword;
+    private TextView regRepeatPass;
+    private TextView regEmail;
+    private TextView regPhone;
     private TextView regTermsAndConditions;
+    private CheckBox regCheckBoxIAgree;
+    private Button regBtnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +28,21 @@ public class RegisterActivity extends AppCompatActivity {
 
         regTermsAndConditions = (TextView)findViewById(R.id.regTerms);
         regTermsAndConditions.setMovementMethod(new ScrollingMovementMethod());
+
+        regUsername = (TextView)findViewById(R.id.regUsername);
+        regPassword = (TextView)findViewById(R.id.regPassword);
+        regRepeatPass = (TextView)findViewById(R.id.regRepeatPassword);
+        regEmail = (TextView)findViewById(R.id.regEmail);
+        regPhone = (TextView)findViewById(R.id.regPhone);
+        regBtnSignUp = (Button) findViewById(R.id.btnLogin);
+
+        regBtnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Intent intent = new Intent(getBaseContext(), RegisterActivity.class);
+//                startActivity(intent);
+            }
+        });
 
 
     }
